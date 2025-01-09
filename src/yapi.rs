dce9a9c67d1bf7711e21d05c8412dc47ce3d6f9e
human9000 <human9000.2021@gmail.com>
2025-01-09 22:07:00 +0500

    added test

--- src/yapi.rs
+++ src/yapi.rs
@@ -15,6 +15,7 @@ pub async fn get_track_info(track_id: usize) -> Result<TrackInfo> {
     let name = data["title"].to_string();
 
     let artists = data["artists"].as_array().unwrap().to_owned();
+    
     let artists: Vec<String> = artists
         .into_iter()
         .map(|val| val["name"].as_str().unwrap().to_owned())
@@ -34,13 +35,14 @@ pub async fn get_track_download_info(track_id: usize) -> Result<String> {
     .await?
     .text()
     .await?;
+    
     let data: Value = serde_json::from_str(&download_info)?;
     let track_url = data["result"][0]["downloadInfoUrl"].as_str().unwrap();
 
     Ok(track_url.to_owned())
 }
 
-#[derive(Debug, serde::Serialize, Clone)]
+#[derive(Debug, serde::Serialize, Clone, PartialEq, Eq)]
 pub struct TrackData {
     pub name: String,
     pub artists: Vec<String>,
@@ -48,9 +50,27 @@ pub struct TrackData {
     pub download_info: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct TrackInfo {
     pub name: String,
     pub artists: Vec<String>,
     pub cover_url: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{get_track_info, TrackInfo};
+
+    #[actix_web::test]
+    async fn test_track_info() {
+        let track_info = get_track_info(127986642).await.unwrap();
+
+        let needed = TrackInfo {
+            name: "nerves".to_string(),
+            artists: vec!["ptasinski".to_string(), "RJ Pasin".to_string()],
+            cover_url: "avatars.yandex.net/get-music-content/9868087/7c2773ac.a.32037431-1/%%".to_string()
+        };
+        
+        assert_eq!(needed, track_info)
+    }
 }

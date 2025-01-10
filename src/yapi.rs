45187671d18f8f9aaa7c112c58c0b797efc3224f
human9000 <human9000.2021@gmail.com>
2025-01-10 11:24:02 +0500

    fixed parsing, cargo t now without errors

--- src/yapi.rs
+++ src/yapi.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use serde_json::Value;
 
 pub async fn get_track_info(track_id: usize) -> Result<TrackInfo> {
@@ -7,17 +7,19 @@ pub async fn get_track_info(track_id: usize) -> Result<TrackInfo> {
         .text()
         .await?;
 
-    let data: Value = serde_json::from_str(&track_info)?;
-    let data = data["result"]["0"].to_owned();
-
-    let cover_url = data["coverUri"].to_string();
-
-    let name = data["title"].to_string();
-
-    let artists = data["artists"].as_array().unwrap().to_owned();
+    let data: Value =
+        serde_json::from_str(&track_info).context("failed to parse the whole json")?;
     
+    let data = data["result"][0].to_owned();
+
+    let cover_url = data["coverUri"].to_string().trim_matches(|c| c == '\\' || c == '"').to_string();
+
+    let name = data["title"].to_string().trim_matches(|c| c == '\\' || c == '"').to_string();
+
+    let artists = data["artists"].as_array().unwrap();
+
     let artists: Vec<String> = artists
-        .into_iter()
+        .iter()
         .map(|val| val["name"].as_str().unwrap().to_owned())
         .collect();
 
@@ -35,7 +37,7 @@ pub async fn get_track_download_info(track_id: usize) -> Result<String> {
     .await?
     .text()
     .await?;
-    
+
     let data: Value = serde_json::from_str(&download_info)?;
     let track_url = data["result"][0]["downloadInfoUrl"].as_str().unwrap();
 
@@ -59,18 +61,22 @@ pub struct TrackInfo {
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Ok;
+
     use super::{get_track_info, TrackInfo};
 
     #[actix_web::test]
-    async fn test_track_info() {
-        let track_info = get_track_info(127986642).await.unwrap();
+    async fn test_track_info() -> Result<(), anyhow::Error> {
+        let track_info = get_track_info(127986642).await?;
 
         let needed = TrackInfo {
             name: "nerves".to_string(),
             artists: vec!["ptasinski".to_string(), "RJ Pasin".to_string()],
-            cover_url: "avatars.yandex.net/get-music-content/9868087/7c2773ac.a.32037431-1/%%".to_string()
+            cover_url: "avatars.yandex.net/get-music-content/9868087/7c2773ac.a.32037431-1/%%"
+                .to_string(),
         };
-        
-        assert_eq!(needed, track_info)
+
+        assert_eq!(needed, track_info);
+        Ok(())
     }
 }
